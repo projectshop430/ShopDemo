b4ef4b8b0267bb71bd200619792c0bb529d118bc
projectshop430 <projectshop430@gmail.com>
2023-10-15 14:28:44 +0330

    Add project files.

---
+++ ShopDemo.Data/Context/ShopDemoContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShopDemo.Data.Entity.Account;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopDemo.Data.Context
+{
+    public class ShopDemoContext : DbContext
+    {
+       
+        public DbSet<User> Users { get; set; }
+
+    }
+}
---
+++ ShopDemo.Data/Entity/Account/User.cs
@@ -0,0 +1,17 @@
+﻿using ShopDemo.Data.Entity.Comment;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopDemo.Data.Entity.Account
+{
+    public class User :BaseEnity<long>
+    {
+        #region property
+        #endregion
+        #region relation
+        #endregion
+    }
+}

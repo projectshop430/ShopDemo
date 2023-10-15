89a7b1c50a395efe97e2ef42c63e1d0951a8f39a
projectshop430 <projectshop430@gmail.com>
2023-10-15 19:03:13 +0330

    8-  انتقال قالب به پروژه و ایجاد کامپوننت های Header و Footer

---
+++ ShopDemo/ViewComponents/SiteViewComponents.cs
@@ -0,0 +1,29 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ShopDemo.ViewComponents
+{
+    #region site header
+
+    public class SiteHeaderViewComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View("SiteHeader");
+        }
+    }
+
+    #endregion
+
+    #region site footer
+
+    public class SiteFooterViewComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View("SiteFooter");
+        }
+    }
+
+    #endregion
+}

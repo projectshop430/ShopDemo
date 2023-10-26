0b524b6ebf892c48e626c3630c76980a174b2834
projectshop430 <projectshop430@gmail.com>
2023-10-26 12:32:03 +0330

    30-پیاده سازی مدل های Ticket

---
+++ ShopDemo.Data/Entity/Contacts/TicketMessage.cs
@@ -0,0 +1,35 @@
+﻿using ShopDemo.Data.Entity.Account;
+using ShopDemo.Data.Entity.Comment;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace ShopDemo.Data.Entity.Contacts
+{
+    public class TicketMessage : BaseEnity
+    {
+        #region properties
+
+        public long TicketId { get; set; }
+
+        public long SenderId { get; set; }
+
+        [Display(Name = "متن پیام")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        public string Text { get; set; }
+
+        #endregion
+
+        #region relations
+
+        public Ticket Ticket { get; set; }
+
+        public User Sender { get; set; }
+
+        #endregion
+    }
+}

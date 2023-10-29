b026f6767af63b57a5335fe9238212bdd8530a42
projectshop430 <projectshop430@gmail.com>
2023-10-29 12:36:46 +0330

    46-انتقال قالب ادمین به پروژه

---
+++ ShopDemo/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ShopDemo.Areas.Admin.Controllers
+{
+    public class HomeController : AdminBaseController
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
